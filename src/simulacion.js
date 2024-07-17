a5014ac84e2c1577dc4484a9a7aefa39b433577c
Manuel <mcarballido17@gmail.com>
2024-07-16 23:52:40 -0300

    nuevo tp

--- src/simulacion.js
+++ src/simulacion.js
@@ -1,168 +1,169 @@
 import { CANTIDAD_DE_FILAS_A_SIMULAR } from './components/SimulacionFormulario'
 
-class TrabajoPractico {
-  constructor(stockInicial, cantidadFilasASimular) {
-    this.STOCK_INICIAL = stockInicial
-    this.CANTIDAD_DE_FILAS_A_SIMULAR = cantidadFilasASimular
-    this.resultados = []
-    this.cantidadLlegadasClientes = 0 // Contador para la cantidad total de llegadas de clientes
-    this.porcentajeClientesTristes = 0
+
+// lugaresDeEstacionamiento = [ 
+//    { tipo: utilitario, ocupados: 0 }, 
+//    { tipo: utilitario, ocupados: 0 }, 
+//    { tipo: peque, ocupados: 0 }
+// ]
+
+// FinDeEstadia: auto35
+// auto35 = { tamaño: peque, lugar: Lugar1{ tipo: utilitario, ocupados: 1 } }
+// auto36 = { tamaño: peque, lugar: Lugar1{ tipo: utilitario, ocupados: 2 } }
+//
+// auto35 = { tamaño: peque, lugar: { tipo: utilitario, ocupados: 0 } }
+//
+// auto35 = { tamaño: peque, lugar: null }
+
+
+// LlegadaVehiculo: auto36
+// auto36 = { tamaño: utilitario, lugar: { tipo: utilitario, ocupados: 0 }, }
+
+// auto35 = { tamaño: peque, lugar: null }
+
+class Auto {
+  constructor(nro, tamano, estado, lugar) {
+    this.nro = nro
+    this.tamano = tamano // pequeño
+    this.estado = estado // estacionado, esperandoPagar, pagando
+    this.lugar = lugar // { tipo: grande }
   }
+}
+
+
+class Lugar {
+  constructor(tamano, ocupados) {
+    this.tamano = tamano // grande, pequeño, utilitario
+    this.ocupados = ocupados // 0, 1, 2
+  }
+}
+
+class TrabajoPractico {
+  // constructor(cantidadFilasASimular) {
+  //   this.CANTIDAD_DE_FILAS_A_SIMULAR = cantidadFilasASimular
+  //   this.resultados = []
+  // }
 
   comenzarEjecucion() {
+    const lugaresPequenos = Array.from({ length: 10 }, () => new Lugar('pequeño', 0))
+    const lugaresGrandes = Array.from({ length: 6 }, () => new Lugar('grande', 0))
+    const lugaresUtilitarios = Array.from({ length: 4 }, () => new Lugar('utilitario', 0))
+
     const datos = {
-      colaEventos: [],
-      empleadosLibres: 2,
-      colaClientes: [],
-      stock: this.STOCK_INICIAL,
-      clientes: 0,
-      clientesTristes: 0,
+      lugaresDeEstacionamiento: [...lugaresPequenos, ...lugaresGrandes, ...lugaresUtilitarios],
+      cajaOcupada: false,
+      cantAutosIngresados: 0,
     }
+    // const datos = {
+    //   colaEventos: [],
+    //   empleadosLibres: 2,
+    //   colaClientes: [],
+    //   stock: this.STOCK_INICIAL,
+    //   clientes: 0,
+    //   clientesTristes: 0,
+    // }
 
-    this.inicializarEventos(datos)
+    // this.inicializarEventos(datos)
 
-    for (let fila = 1; fila < this.CANTIDAD_DE_FILAS_A_SIMULAR; fila++) {
-      const eventoInminente = this.encontrarEventoMasProximo(datos)
+    // for (let fila = 1; fila < this.CANTIDAD_DE_FILAS_A_SIMULAR; fila++) {
+    //   const eventoInminente = this.encontrarEventoMasProximo(datos)
 
-      eventoInminente.ocurreEvento(datos)
+    //   eventoInminente.ocurreEvento(datos)
 
-      const filaDatos = {
-        tiempo: eventoInminente.tiempo,
-        evento: eventoInminente.constructor.name,
-        nroCliente: eventoInminente.nroCliente,
-        stock: datos.stock,
-        empleadosLibres: datos.empleadosLibres,
-        colaClientes: [...datos.colaClientes],
-        eventosCola: datos.colaEventos.map(evento => ({
-          tiempo: evento.tiempo,
-          evento: evento.constructor.name,
-          nroCliente: evento.nroCliente,
-        })),
-      }
+    //   const filaDatos = {
+    //     tiempo: eventoInminente.tiempo,
+    //     evento: eventoInminente.constructor.name,
+    //     nroCliente: eventoInminente.nroCliente,
+    //     stock: datos.stock,
+    //     empleadosLibres: datos.empleadosLibres,
+    //     colaClientes: [...datos.colaClientes],
+    //     eventosCola: datos.colaEventos.map(evento => ({
+    //       tiempo: evento.tiempo,
+    //       evento: evento.constructor.name,
+    //       nroCliente: evento.nroCliente,
+    //     })),
+    //   }
 
-      if (fila >= this.FILA_A_SIMULAR_DESDE && fila < this.FILA_A_SIMULAR_DESDE + this.CANTIDAD_FILAS_A_MOSTRAR) {
-        this.resultados.push({ ...filaDatos, nroFila: fila })
-      }
-    }
+    //   if (fila >= this.FILA_A_SIMULAR_DESDE && fila < this.FILA_A_SIMULAR_DESDE + this.CANTIDAD_FILAS_A_MOSTRAR) {
+    //     this.resultados.push({ ...filaDatos, nroFila: fila })
+    //   }
+    // }
   }
 
-  encontrarEventoMasProximo(datos) {
-    const eventoEncontrado = datos.colaEventos.reduce(
-      (eventoMinimo, evento) => (evento.tiempo < eventoMinimo.tiempo ? evento : eventoMinimo),
-      datos.colaEventos[0]
-    )
+  // encontrarEventoMasProximo(datos) {
+  //   const eventoEncontrado = datos.colaEventos.reduce(
+  //     (eventoMinimo, evento) => (evento.tiempo < eventoMinimo.tiempo ? evento : eventoMinimo),
+  //     datos.colaEventos[0]
+  //   )
 
-    datos.colaEventos = datos.colaEventos.filter(evento => evento !== eventoEncontrado)
+  //   datos.colaEventos = datos.colaEventos.filter(evento => evento !== eventoEncontrado)
 
-    return eventoEncontrado
-  }
+  //   return eventoEncontrado
+  // }
 
-  inicializarEventos(datos) {
-    datos.colaEventos.push(new LlegadaCliente(0, 1))
+  // inicializarEventos(datos) {
 
-    if (datos.stock === 0) {
-      datos.colaEventos.push(new FinCoccionHorno(0, 0))
-    } else {
-      datos.colaEventos.push(new EncendidoHorno45Minutos(0))
-    }
-  }
+  // }
 
-  getResultados() {
-    return this.resultados
+  // getResultados() {
+  //   return this.resultados
+  // }
+}
+
+function tamanoDeAuto(random) {
+  if (random < 0.6) {
+    return 'pequeño'
+  } else if (random < 0.85) {
+    return 'grande'
+  } else {
+    return 'utilitario'
   }
 }
 
-class LlegadaCliente {
-  constructor(tiempoActual, nroCliente) {
-    this.tiempo = tiempoActual + -3 * Math.log(1 - Math.random())
-    this.nroCliente = nroCliente
+class EventoLlegadaAuto {
+  constructor(tiempoActual) { // constructor es cuando creamos el evento
+    this.randomTiempo = Math.random()
+    this.tiempoDeOcurrencia = tiempoActual + 12 + this.randomTiempo * (14 - 12)
   }
 
   ocurreEvento(datos) {
-    datos.clientes += 1
-    datos.colaEventos.push(new LlegadaCliente(this.tiempo, datos.clientes + 1))
+    const nroAuto = datos.cantAutosIngresados + 1
+    const randomTamano = Math.random()
+    const tamano = tamanoDeAuto(randomTamano) // grande
+    
+    const autoQueLlega = new Auto(nroAuto, tamano, 'estacionado')
+    
+    for (let i = 0; i < datos.lugaresDeEstacionamiento.length; i++) {
+      const lugarEncontrado = datos.lugaresDeEstacionamiento[i]
 
-    if (datos.stock === 0) {
-      datos.clientesTristes += 1
-    } else if (datos.empleadosLibres > 0) {
-      datos.colaEventos.push(new FinAtencion(this.tiempo, this.nroCliente))
-      datos.empleadosLibres -= 1
-    } else {
-      datos.colaClientes.push(this.nroCliente)
-      datos.colaEventos.push(new ClienteSeRetiraPorTiempo(this.tiempo, this.nroCliente))
-    }
-  }
-}
-
-class FinAtencion {
-  constructor(tiempoActual, nroCliente) {
-    this.tiempo = tiempoActual + (0.5 + Math.random())
-    this.nroCliente = nroCliente
-  }
-
-  ocurreEvento(datos) {
-    const productosPedidos = Math.floor(Math.random() * 3) + 1
-
-    if (datos.stock === 0) {
-      datos.clientesTristes += 1
-    } else {
-      datos.stock = productosPedidos > datos.stock ? 0 : datos.stock - productosPedidos
-
-      if (datos.stock === 0) {
-        datos.clientesTristes += datos.colaClientes.length
-        datos.colaClientes = []
-        datos.colaEventos = datos.colaEventos.filter(evento => !(evento instanceof EncendidoHorno45Minutos))
-        datos.colaEventos.push(new FinCoccionHorno(this.tiempo, 0))
+      if (lugarEncontrado.tamano === tamano && lugarEncontrado.ocupados === 0) {
+        lugarEncontrado.ocupados = 1
+        autoQueLlega.lugar = lugarEncontrado
       }
+      
     }
 
-    if (datos.colaClientes.length > 0) {
-      const nroCliente = datos.colaClientes.shift()
-      datos.colaEventos.push(new FinAtencion(this.tiempo, nroCliente))
-      datos.colaEventos = datos.colaEventos.filter(
-        evento => !(evento instanceof ClienteSeRetiraPorTiempo && evento.nroCliente === nroCliente)
-      )
-    } else {
-      datos.empleadosLibres += 1
-    }
   }
 }
 
-class FinCoccionHorno {
-  constructor(tiempoActual, stock) {
-    if (stock === 0) {
-      this.produccion = 45
-      this.tiempo = tiempoActual + 10.9
-    } else {
-      this.produccion = 30
-      this.tiempo = tiempoActual + 9.2
-    }
-  }
-
-  ocurreEvento(datos) {
-    datos.stock += this.produccion
-    datos.colaEventos.push(new EncendidoHorno45Minutos(this.tiempo))
-  }
-}
-
-class EncendidoHorno45Minutos {
+class EventoFinCobro {
   constructor(tiempoActual) {
-    this.tiempo = tiempoActual + 45
+    this.tiempo = tiempoActual + 2
   }
 
   ocurreEvento(datos) {
-    datos.colaEventos.push(new FinCoccionHorno(this.tiempo, datos.stock))
-  }
-}
+    // datos.clientes += 1
+    // datos.colaEventos.push(new LlegadaCliente(this.tiempo, datos.clientes + 1))
 
-class ClienteSeRetiraPorTiempo {
-  constructor(tiempoActual, nroCliente) {
-    this.tiempo = tiempoActual + 5
-    this.nroCliente = nroCliente
-  }
-
-  ocurreEvento(datos) {
-    datos.colaClientes = datos.colaClientes.filter(cliente => cliente !== this.nroCliente)
+    // if (datos.stock === 0) {
+    //   datos.clientesTristes += 1
+    // } else if (datos.empleadosLibres > 0) {
+    //   datos.colaEventos.push(new FinAtencion(this.tiempo, this.nroCliente))
+    //   datos.empleadosLibres -= 1
+    // } else {
+    //   datos.colaClientes.push(this.nroCliente)
+    //   datos.colaEventos.push(new ClienteSeRetiraPorTiempo(this.tiempo, this.nroCliente))
+    // }
   }
 }
 
